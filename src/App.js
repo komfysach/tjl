e3c3435abd48cc0f1f0861d8416a0e1bbe612a2a
komfysach <sach@getkomfy.net>
2021-08-03 17:30:34 +0200

    adding header component, and links to restaurant page and bookings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.scss';
 import HomePage from './pages/homepage/homepage.component';
 import RestaurantPage from './pages/restaurant/restaurant.component';
+import Header from './components/header/header.component';
 import { Switch, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div class="App">
+      {/* Header Component*/}
+      <Header />
       {/* Switch Component*/}
       <Switch>
         {/* Route to Home Page Component*/}
